a795bcb6629b0732c779d95a460fbac1f9807ced
Steve <Stevegreen22@me.com>
2019-01-30 21:37:32 +0000

    Routes and tempaltes functional

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+
+# debugging
+import pdb
+import code
+
 app = Flask(__name__)
 
 
@@ -14,8 +20,29 @@ def members():
 
 @app.route("/members/<name>/")
 def getMember(name):
-    return name
+    return render_template(
+        'test.html', name=name)
+
+
+@app.route("/hello/<name>/")
+def hello(name):
+    return render_template(
+        'hello.html', name=name)
+
+
+@app.route("/quotes/<name>/")
+def passVariables(name):
+    quotes = ["'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
+              "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
+              "'To understand recursion you must first understand recursion..' -- Unknown",
+              "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
+              "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
+              "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"]
+    randomNumber = randint(0, len(quotes)-1)
+    quote = quotes[randomNumber]
+
+    return render_template('quotes.html', **locals())
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run()
