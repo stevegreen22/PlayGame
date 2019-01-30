2655093146505f441a7d53d8ba4ca8ef14ed85cc
Steve <Stevegreen22@me.com>
2019-01-30 20:06:00 +0000

    testing initial routes

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "index!"
+
+
+@app.route("/members")
+def members():
+    return "Members"
+
+
+@app.route("/members/<name>/")
+def getMember(name):
+    return name
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
